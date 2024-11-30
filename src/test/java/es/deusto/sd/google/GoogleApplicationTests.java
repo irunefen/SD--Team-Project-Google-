81dbe1df0fd3786942cad83ba7c44ee0d90871f4
jaimeetxebarria <jaimeetxebarria@gmail.com>
2024-11-30 15:04:34 +0100

    Initial commit

---
+++ src/test/java/es/deusto/sd/google/GoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package es.deusto.sd.google;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
